ac53bd3b90799004defbf938f9825fefff78a771
gehanmostafa <gm309861@gmail.com>
2024-12-17 23:01:02 +0200

    Add : Initial commit

---
+++ src/app/(protected)/home/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const page = () => {
+  return (
+    <div>
+      Home Page 
+      <Button>Click Here </Button>
+    </div>
+  )
+}
+
+export default page
